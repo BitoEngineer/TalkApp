dc12c55d45c4e83f619ca8e7c836b32f4fbe7b53
BitoEngineer <bito_asd@hotmail.com>
2019-05-25 21:14:16 +0100

    TalkApp done - all main functionalities working

---
+++ Assets/Scripts/ChatScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatScript : MonoBehaviour
+{
+    public GameObject MeMessage;
+    public GameObject YouMessage;
+    public GameObject MessageContainer;
+    public GameObject InputField;
+
+    private int i = 0;
+    public void OnTextEnded(string txt)
+    {
+        var go = Instantiate(i++ % 2 == 0 ? MeMessage : YouMessage, MessageContainer.transform);
+        go.GetComponentInChildren<Text>().text = txt;
+
+        //InputField.GetComponentInChildren<InputField>().text = "";
+        //InputField.GetComponentInChildren<Text>().text = "";
+        InputField.GetComponentInChildren<InputField>().ActivateInputField();
+    }
+}
---
+++ Assets/Scripts/TalkScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TalkScript : MonoBehaviour
+{
+    public void TalkButonListener()
+    {
+        SceneManager.LoadScene("ChatScene");
+    }
+}
